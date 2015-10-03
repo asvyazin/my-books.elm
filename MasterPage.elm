b662c4c546b46e963f1bce1f1868f925a56c8056
Alexander Svyazin <guybrush@live.ru>
2015-10-03 21:31:13 +0500

    using jspm

--- MasterPage.elm
+++ MasterPage.elm
@@ -7,9 +7,4 @@ import Html.Shorthand exposing (..)
 
 view : List Html -> Html
 view children =
-  let
-    bootstrapCss = node "link" [A.href "/bower_components/bootstrap/dist/css/bootstrap.min.css", A.rel "stylesheet"] []
-    bootstrapJs = node "script" [A.src "/bower_components/bootstrap/dist/js/bootstrap.min.js"] []
-  in 
-    div' { class = "container" }
-           (bootstrapCss :: bootstrapJs :: children)
+  div' { class = "container" } children
